97cad7246264a9a6d5d25698e2fc457b3a6f3f24
Bradley Morris <bradleymorrisza@gmail.com>
2018-07-21 00:29:48 +0200

    Added jest config to mock out scss styles in tests, they cause failures

---
+++ __mocks__/fileMock.js
@@ -0,0 +1,2 @@
+//mock files based on this post https://github.com/facebook/jest/issues/3094
+module.exports = "test-file-stub";
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   setupFiles: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"]
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  }
 };
