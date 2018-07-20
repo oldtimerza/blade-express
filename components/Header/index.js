69b92494274f36c23db18bbd8f1847eeebdde6d1
Bradley Morris <bradleymorrisza@gmail.com>
2018-07-21 00:01:32 +0200

    Fixed Hamburger menu for mobile

--- components/Header/index.js
+++ components/Header/index.js
@@ -28,18 +28,28 @@ class Header extends Component {
 
   render() {
     return (
-      <Navbar expand="md">
+      <Navbar className={`${css.navbar} py-0`} expand="md">
         <NavbarBrand href="/">
           <img src="../../static/images/store_logo.png" />
         </NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
+        <NavbarToggler onClick={this.toggle} className={css.navbar_toggler}>
+          <div
+            className={`hamburger hamburger--slider ${
+              this.state.isOpen ? "is-active" : ""
+            }`}
+          >
+            <div className="hamburger-box">
+              <div className="hamburger-inner" />
+            </div>
+          </div>
+        </NavbarToggler>
         <Collapse isOpen={this.state.isOpen} navbar>
           <Nav className="mr-auto" navbar>
             {this.props.menus &&
             this.props.menus.items &&
             this.props.menus.items.length
               ? this.props.menus.items.map(item => (
-                  <NavItem key={item.id}>
+                  <NavItem key={item.id} className={css.navbar_link}>
                     <NavLink href={item.url} className={css.navbar_link}>
                       {item.title}
                     </NavLink>
--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
+          <link rel="stylesheet" href="/static/css/hamburgers.css" />
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
