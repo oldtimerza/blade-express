777bf0c60f7b8b2f1b5e6c407fd40941ac701e19
Bradley Morris <bradleymorrisza@gmail.com>
2018-07-20 22:27:52 +0200

    Added viewport meta tag and mobile bootstrap menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,28 +1,53 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+  Collapse
+} from "reactstrap";
 
 import css from "./styling.scss";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      toggle: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
   render() {
     return (
-      <Navbar className={css.navbar} expand="md">
+      <Navbar expand="md">
         <NavbarBrand href="/">
           <img src="../../static/images/store_logo.png" />
         </NavbarBrand>
-        <Nav className="mr-auto">
-          {this.props.menus &&
-          this.props.menus.items &&
-          this.props.menus.items.length
-            ? this.props.menus.items.map(item => (
-                <NavItem key={item.id}>
-                  <NavLink href={item.url} className={css.navbar_link}>
-                    {item.title}
-                  </NavLink>
-                </NavItem>
-              ))
-            : null}
-        </Nav>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            {this.props.menus &&
+            this.props.menus.items &&
+            this.props.menus.items.length
+              ? this.props.menus.items.map(item => (
+                  <NavItem key={item.id}>
+                    <NavLink href={item.url} className={css.navbar_link}>
+                      {item.title}
+                    </NavLink>
+                  </NavItem>
+                ))
+              : null}
+          </Nav>
+        </Collapse>
       </Navbar>
     );
   }
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
