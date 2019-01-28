7b4642711f37d096bd828761edd52847fa5259c6
Bradley Morris <bradleymorrisza@gmail.com>
2019-01-28 23:33:09 +0200

    fixed broken test

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -34,18 +34,18 @@ describe("index page", () => {
   it("should render with results and popular visas", () => {
     const mountedHomePage = mount(<HomePage {...props} />);
     const mediaCards = mountedHomePage.find(MediaCard);
-    expect(mediaCards.length).toBe(4);
-    expect(mediaCards.at(1).props()).toEqual({
+    expect(mediaCards.length).toBe(3);
+    expect(mediaCards.at(0).props()).toEqual({
       imageUrl: "image1",
       title: "first",
       text: "description1"
     });
-    expect(mediaCards.at(2).props()).toEqual({
+    expect(mediaCards.at(1).props()).toEqual({
       imageUrl: "image2",
       title: "second",
       text: "description2"
     });
-    expect(mediaCards.at(3).props()).toEqual({
+    expect(mediaCards.at(2).props()).toEqual({
       imageUrl: "image3",
       title: "third",
       text: "description3"
