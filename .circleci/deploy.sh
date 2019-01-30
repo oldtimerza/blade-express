5dccd90280a15fc6158f72a34e7784f0e30fc2c8
Bradley Morris <bradleymorrisza@gmail.com>
2019-01-30 21:05:53 +0200

    added some logging info to deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,4 +1,7 @@
-docker stop app && docker rm $(docker ps -a -q) && \ 
+echo stop docker running container and remove old images
+docker stop app && \
+docker rm $(docker ps -a -q) && \ 
 docker rmi $(docker images -q) && \
+echo pull latest docker image and run it
 docker pull oldtimerza/blade-express && \
 docker run -d -t -p 80:3000 --name app oldtimerza/blade-express
