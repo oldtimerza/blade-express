33332c1992c6fcd51c391a8b0112bfcb8c98feec
Bradley Morris <bradleymorrisza@gmail.com>
2019-02-01 00:09:32 +0200

    Testing kubernetes deployment

---
+++ .circleci/kubernetes.sh
@@ -0,0 +1,3 @@
+kubectl --kubectlconfig="blade-express-cluster-kubeconfig.yaml" get nodes && \
+kubectl run blade-express --image=oldtimerza/blade-express:latest --port=8080 && \
+kubectl expose deployment/blade-express --type="NodePort" --port=8080
