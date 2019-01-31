6a1e0642e5924dffe45d417cacd3caf457011420
Bradley Morris <bradleymorrisza@gmail.com>
2019-02-01 00:37:18 +0200

    WIP kubernetes setup continued

--- .circleci/setup-kubectl.sh
+++ .circleci/setup-kubectl.sh
@@ -1,5 +1,3 @@
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+sudo chmod +x kubectl
+sudo mv kubectl /usr/bin
