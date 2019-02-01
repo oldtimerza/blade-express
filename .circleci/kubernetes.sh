3c0ac26a8d9ccee2f9446aa3e70dedc7bb2aba75
Bradley Morris <bradleymorrisza@gmail.com>
2019-02-01 03:04:11 +0200

    WIP kubernets setup

--- .circleci/kubernetes.sh
+++ .circleci/kubernetes.sh
@@ -1,5 +1,5 @@
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" delete deployment blade-express && \
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" delete services blade-express && \
-kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./circleci/deployment.yaml
-kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./circleci/load-balancer.yaml
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./.circleci/deployment.yaml
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./.circleci/load-balancer.yaml
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" get services
