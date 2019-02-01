82dfd73312da0a1608aa3815b8ecff654de8668d
Bradley Morris <bradleymorrisza@gmail.com>
2019-02-01 03:01:08 +0200

    WIP kubernets setup

--- .circleci/kubernetes.sh
+++ .circleci/kubernetes.sh
@@ -1,3 +1,5 @@
-kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" set image deployment.apps/blade-express blade-express=oldtimerza/blade-express:latest
-kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" expose deployment.apps/blade-express --type="LoadBalancer" --port 8080
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" delete deployment blade-express
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" delete services blade-express
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./circleci/deployment.yaml
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./circleci/load-balancer.yaml
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" get services
