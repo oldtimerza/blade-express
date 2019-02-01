6b4d8033d03bf0e7bd6788b47cb7451754feb69e
Bradley Morris <bradleymorrisza@gmail.com>
2019-02-01 02:20:53 +0200

    WIP kubernets setup

--- .circleci/kubernetes.sh
+++ .circleci/kubernetes.sh
@@ -1,2 +1,3 @@
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" get nodes && \
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./.circleci/manifest.yaml
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" expose deployment.apps/blade-express --type="NodePort" --port 8080
