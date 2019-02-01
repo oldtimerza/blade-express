f4ee7e6a7a8974b289eef14f9a3ffdb803cbae37
Bradley Morris <bradleymorrisza@gmail.com>
2019-02-01 23:45:52 +0200

    added attempt to create loadbalancer each deploy

--- .circleci/kubernetes.sh
+++ .circleci/kubernetes.sh
@@ -1,3 +1,4 @@
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" delete deployment blade-express && \
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./.circleci/deployment.yaml
+kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" create -f ./.circleci/load-balancer.yaml
 kubectl --kubeconfig="./.circleci/blade-express-cluster-kubeconfig.yaml" get services
